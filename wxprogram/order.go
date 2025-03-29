bb56a7356dcb5a44fb5dd65171d94b1fa9c1e15a
zohu <38601989@qq.com>
2025-03-29 09:45:35 +0800

    create

---
+++ wxprogram/order.go
@@ -0,0 +1,50 @@
+package wxprogram
+
+import (
+	"github.com/zohu/zwx"
+	"time"
+)
+
+type UploadShippingInfoOrderKey struct {
+	OrderNumberType int `json:"order_number_type"`
+}
+type UploadShippingInfoShippingItem struct {
+	ItemDesc string `json:"item_desc"`
+}
+type UploadShippingInfoPayer struct {
+	Openid string `json:"openid"`
+}
+type ParamUploadShippingInfo struct {
+	OrderKey      UploadShippingInfoOrderKey       `json:"order_key"`
+	LogisticsType int                              `json:"logistics_type"`
+	DeliveryMode  int                              `json:"delivery_mode"`
+	ShippingList  []UploadShippingInfoShippingItem `json:"shipping_list"`
+	UploadTime    string                           `json:"upload_time"`
+	Payer         UploadShippingInfoPayer          `json:"payer"`
+}
+
+func (c *Context) UploadShippingInfo(openid, itemName string) error {
+	var resp zwx.WxResponse
+	if err := zwx.NewHttp(zwx.MethodPost, zwx.ApiWxa.WithPath("sec/order/upload_shipping_info")).
+		SetAccessToken(c.AccessToken()).
+		SetJson(&ParamUploadShippingInfo{
+			OrderKey:      UploadShippingInfoOrderKey{OrderNumberType: 1},
+			LogisticsType: 3,
+			DeliveryMode:  1,
+			ShippingList:  []UploadShippingInfoShippingItem{{ItemDesc: itemName}},
+			UploadTime:    time.Now().Format(time.RFC3339),
+			Payer:         UploadShippingInfoPayer{Openid: openid},
+		}).
+		BindJson(&resp).
+		Debug(c.IsDebug(), c.Logger()).
+		Do(); err != nil {
+		return c.Error("upload_shipping_info", err.Error())
+	}
+	if resp.Errcode != 0 {
+		if c.RetryAccessToken(resp.Errcode) {
+			return c.UploadShippingInfo(openid, itemName)
+		}
+		return c.Error("get_phone_number", resp.Errmsg)
+	}
+	return nil
+}
